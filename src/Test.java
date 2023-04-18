4ce74a135c5ab208b55b69c70fc351b98618c6a8
bazhukovsi <bazhukovsi@yandex.ru>
2023-04-18 10:53:01 +0300

    Update Test

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,8 @@ public class Test {
         for (int i = 0; i < 20; i++) {
             System.out.println("Hello " + i);
         }
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hello " + i);
+        }
     }
 }
